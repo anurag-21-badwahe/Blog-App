74a477e34620bb76af6667ca6873f3805885d851
anurag <anuragbadwahe@gmail.com>
2024-03-26 23:41:52 +0530

    Signin Controller Setup

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const userRoute = require("./routes/user");
 const bodyParser = require("body-parser");
 const connectDB = require("./db")
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
 
